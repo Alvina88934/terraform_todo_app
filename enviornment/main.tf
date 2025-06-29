6eabc2be3a2d0cd0b5e706eeb7352bf24be9da87
Alvina88934 <alvinaadnan88@gmail.com>
2025-06-29 07:36:41 +0100

    added rg

--- enviornment/main.tf
+++ enviornment/main.tf
@@ -3,3 +3,8 @@ module "azurerm_resource_group" {
   resource_group_name     = "todo_app_rg"
   resource_group_location = "West US"
 }
+module "azurerm_resource_group1" {
+  source                  = "../Modules/azurerm_resource_group1"
+  resource_group_name     = "todo_app_rg1"
+  resource_group_location = "West US"
+}
